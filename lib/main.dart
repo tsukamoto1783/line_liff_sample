34b12a7d3972813e5cbb622d88c27058c2d9c25e
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-08-19 11:18:25 +0900

    liff test:Confirmation of getprofile() operation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_line_liff/flutter_line_liff.dart';
+import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 
-void main() {
+var userInfo;
+
+Future<void> main() async {
+  await FlutterLineLiff().init(
+    config: Config(liffId: '1657390584-p2Ar7vyL'),
+    successCallback: () {
+      debugPrint('LIFF init success.');
+    },
+    errorCallback: (error) {
+      debugPrint(
+          'LIFF init error: ${error.name}, ${error.message}, ${error.stack}');
+    },
+  );
+  // FlutterLineLiff().ready.then((_) {
+  //   // SDK is ready now.
+  //   final JWTPayload? decodedIDToken = FlutterLineLiff().getDecodedIDToken();
+  //   debugPrint(decodedIDToken.toString());
+  //
+  //   final String? id = FlutterLineLiff().id;
+  //   debugPrint(id);
+  //
+  //   final String? idToken = FlutterLineLiff().getIDToken();
+  //   debugPrint(idToken);
+  //
+  //   debugPrint('ready ok');
+  // });
+  //
+  // final Profile profile = await FlutterLineLiff().profile;
+  // debugPrint(profile.displayName);
+  // debugPrint('test1');
   runApp(const MyApp());
 }
 
@@ -13,15 +45,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,16 +54,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,52 +62,56 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Profile? userInfo;
+
+  Future<void> getProfiles() async {
+    userInfo = await FlutterLineLiff().profile;
+    debugPrint(userInfo.toString());
+    debugPrint('test1');
+  }
+
+  @override
+  void initState() {
+    FlutterLineLiff().ready.then((_) {});
+    super.initState();
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    debugPrint('test2');
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            FutureBuilder(
+              future: getProfiles(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return Text("Error: ${snapshot.error}");
+                  }
+                  if (!snapshot.hasData) {
+                    return Text("データが見つかりません");
+                  }
+                  // データ表示
+                  return Text('${snapshot.data}');
+                } else {
+                  // 処理中の表示
+                  return const CircularProgressIndicator();
+                }
+              },
+            ),
+            Text(FlutterLineLiff().id!),
             const Text(
               'You have pushed the button this many times:',
             ),
