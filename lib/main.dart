61b4aead26e9dd6e2381fbb3363ed3191a4bf67a
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-09-06 20:04:35 +0900

    For sample code (to be reverted later)

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_line_liff/flutter_line_liff.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-JWTPayload? decodedIDToken;
-
+Profile? userInfo;
 Future<void> main() async {
   await dotenv.load(fileName: "env");
   await FlutterLineLiff().init(
     config: Config(liffId: dotenv.env['LIFFID_KEY'].toString()),
     successCallback: () {
-      decodedIDToken = FlutterLineLiff().getDecodedIDToken();
       debugPrint('LIFF init success.');
     },
     errorCallback: (error) {
@@ -17,6 +15,7 @@ Future<void> main() async {
           'LIFF init error: ${error.name}, ${error.message}, ${error.stack}');
     },
   );
+  userInfo = await FlutterLineLiff().profile;
   runApp(const MyApp());
 }
 
@@ -26,28 +25,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (userInfo == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('LINE LIFF sample app'),
+        ),
+        body: const Center(
+          child: Text("decodedIDToken: null"),
+        ),
+      );
+    } else {
+      return Scaffold(
         appBar: AppBar(
           title: const Text('LINE LIFF sample app'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              const Text("↓Profile Picture"),
-              Image.asset(
-                'images/cat_sample.jpeg',
-                width: 200.0,
-                height: 200.0,
-              ),
-              const Text("User Name:           ゆーと"),
-              const Text("User ID:                  TestLineID123456789"),
-              const Text("Status Message: ステータスメッセージ(設定してるなら)"),
+              // // =========== getDecodeIDToken Ver. ===========
+              // const Text("Profile Picture"),
+              // Image.network(decodedIDToken!.picture.toString()),
+              // Text("User Name: ${decodedIDToken!.name.toString()}"),
+
+              // =========== getProfile Ver. ===========
+              const Text("Profile Picture"),
+              Image.network(userInfo!.pictureUrl.toString()),
+              Text("User Name: ${userInfo!.displayName}"),
+              Text("User ID: ${userInfo!.userId}"),
+              Text("Status Message: ${userInfo!.statusMessage}"),
             ],
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
