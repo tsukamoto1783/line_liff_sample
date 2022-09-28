e40db8929ef6733f4961db65b29fee895e909389
Yuto <61080605+matttake@users.noreply.github.com>
2022-09-28 23:08:40 +0900

    Revert "Check initialization process in js package"

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
-import 'dart:js_util';
 import 'package:flutter/material.dart';
 import 'package:flutter_line_liff/flutter_line_liff.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'liff_init.dart';
 
-// jsパッケージ版でinit()までしてみる。
-JWTPayload? decodedIDToken;
+Profile? userInfo;
 Future<void> main() async {
   // KEYを定義したenvファイル読み込み
   await dotenv.load(fileName: "env");
   // LIFF初期化処理
-  decodedIDToken =
-      await promiseToFuture(init(dotenv.env['LIFFID_KEY'].toString()));
-  debugPrint(decodedIDToken.toString());
-  await Future.delayed(const Duration(seconds: 2));
+  await FlutterLineLiff().init(
+    config: Config(liffId: dotenv.env['LIFFID_KEY'].toString()),
+    successCallback: () {
+      debugPrint('LIFF init success.');
+    },
+    errorCallback: (error) {
+      debugPrint(
+          'LIFF init error: ${error.name}, ${error.message}, ${error.stack}');
+    },
+  );
+  // ユーザ情報の取得
+  userInfo = await FlutterLineLiff().profile;
   runApp(const MyApp());
 }
 
@@ -27,58 +32,38 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'LINE LIFF sample app'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('LINE LIFF sample app'),
+        ),
+        body: const HomePage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Profile? userInfo;
-
-  @override
-  void initState() {
-    debugPrint('initState start');
-    super.initState();
-
-    Future(() async {
-      userInfo = await FlutterLineLiff().profile;
-      setState(() {});
-      debugPrint('get Profile success');
-    });
-
-    debugPrint('initState end');
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    debugPrint('return Scaffold start');
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+    if (userInfo == null) {
+      return const Center(
+        child: Text("decodedIDToken: null"),
+      );
+    } else {
+      return Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text("=========== getDecodeIDToken ==========="),
-            (decodedIDToken == null)
-                ? const Text("decodedIDToken: null")
-                : Text("user name: ${decodedIDToken!.name.toString()}"),
-            (decodedIDToken == null)
-                ? const Text("")
-                : Image.network(decodedIDToken!.picture.toString()),
-            // Text(FlutterLineLiff().id!),
+            const Text("Profile Picture"),
+            Image.network(userInfo!.pictureUrl.toString()),
+            Text("User Name: ${userInfo!.displayName}"),
+            Text("User ID: ${userInfo!.userId}"),
+            Text("Status Message: ${userInfo!.statusMessage}"),
           ],
         ),
-      ),
-    );
+      );
+    }
   }
 }
